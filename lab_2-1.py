8bd483f8012a171aab9afe9a1ec23c3014528404
p22melsheikh <p22melsheikh@fairfieldprep.org>
2021-09-28 10:06:59 -0400

    labs

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+# Author: MEE 09/28/21
+
+x = input("input an integer: ")
+
+if int(x) % 2 == 0:
+    print(x + "is an even an number")
---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Author: MEE 09/28/21
+
+x = input(" input an integer: ")
+
+if int(x) % 2 == 0:
+    print(x + " is an even an number ")
+else:
+    print("x is a negative number.")
