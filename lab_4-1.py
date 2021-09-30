0d41ca5633c3e8e6a038ba28cfdd492005b31e2b
p22melsheikh <p22melsheikh@fairfieldprep.org>
2021-09-30 11:07:13 -0400

    lab

---
+++ lab_4-1.py
@@ -0,0 +1,7 @@
+# Author MEE 09/30/21
+
+if magic_charge >= 90 and shield_charge >= 75: 
+    print("You defeated the dragon! But the princess is in another castle.")
+else:
+    print("The dragon burns you to a crisp.")
+
